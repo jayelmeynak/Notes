51c850868ceaba2b387c6ddc3c1f1df055fbb350
magomed03 <magomed_mazhitov_03@mail.ru>
2024-05-18 06:46:35 +0300

    Change RecyclerViewAdapter to ListAdapter

---
+++ app/src/main/java/com/example/note/presentation/NoteDiffCallback.kt
@@ -0,0 +1,14 @@
+package com.example.note.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.note.domain.Note
+
+class NoteDiffCallback: DiffUtil.ItemCallback<Note>() {
+    override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/com/example/note/presentation/NoteListAdapter.kt
+++ app/src/main/java/com/example/note/presentation/NoteListAdapter.kt
@@ -1,17 +1,17 @@
-package com.example.note.presentation.adapter
+package com.example.note.presentation
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.note.R
 import com.example.note.databinding.NoteLayoutBinding
-import com.example.note.presentation.fragments.HomeFragmentDirections
 import com.example.note.domain.Note
 
-class NoteAdapter(): RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
-    private var noteList = emptyList<Note>()
+class NoteListAdapter(): ListAdapter<Note,NoteListAdapter.NoteViewHolder>(NoteDiffCallback()) {
+
+    var onNoteClickListener: ((Note) -> Unit)? = null
 
     class NoteViewHolder(view: View): RecyclerView.ViewHolder(view){
         val binding = NoteLayoutBinding.bind(view)
@@ -29,21 +29,12 @@ class NoteAdapter(): RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
         return NoteViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return noteList.size
-    }
-
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        val note = noteList[position]
+        val note = getItem(position)
         holder.bind(note)
         holder.itemView.setOnClickListener{
-            val action = HomeFragmentDirections.actionHomeFragmentToEditNoteFragment(note)
-            holder.itemView.findNavController().navigate(action)
+            onNoteClickListener?.invoke(note)
         }
     }
 
-    fun setData(users: List<Note>) {
-        this.noteList = users
-        notifyDataSetChanged()
-    }
 }
