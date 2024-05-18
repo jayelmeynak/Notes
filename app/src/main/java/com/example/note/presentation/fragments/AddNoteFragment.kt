a37ef088555b73c1ee7468510b68121387ba26c7
magomed03 <magomed_mazhitov_03@mail.ru>
2024-05-18 20:57:57 +0300

    Working version of AddNoteFragment, but there is a bug

--- app/src/main/java/com/example/note/presentation/fragments/AddNoteFragment.kt
+++ app/src/main/java/com/example/note/presentation/fragments/AddNoteFragment.kt
@@ -1,9 +1,6 @@
 package com.example.note.presentation.fragments
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -20,11 +17,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.note.presentation.MainViewModel
 import com.example.note.R
 import com.example.note.databinding.FragmentAddNoteBinding
 import com.example.note.presentation.DialogManager
-import com.example.note.domain.Note
+import com.example.note.presentation.MainViewModel
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -64,25 +60,6 @@ class AddNoteFragment : Fragment(), MenuProvider {
             hideKeyboard()
         }
         binding.tvAddEditTimeDate.text = getCurrentDateTime()
-
-//        val rootView = binding.root
-//        val listener = ViewTreeObserver.OnGlobalLayoutListener {
-//            val rect = Rect()
-//            rootView.getWindowVisibleDisplayFrame(rect)
-//            val screenHeight = rootView.height
-//            val keypadHeight = screenHeight - rect.bottom
-//            val paramsDone = binding.bAddDoneEdit.layoutParams as ConstraintLayout.LayoutParams
-//            if (keypadHeight > screenHeight * 0.15) {
-//                paramsDone.bottomMargin = keypadHeight + (rect.bottom * 0.15).toInt()
-//            } else {
-//                paramsDone.bottomMargin = (rect.bottom * 0.05).toInt()
-//            }
-//
-//            binding.bAddDoneEdit.layoutParams = paramsDone
-//        }
-//        rootView.viewTreeObserver.addOnGlobalLayoutListener(listener)
-
-
     }
 
     private fun hideKeyboard() {
@@ -93,10 +70,8 @@ class AddNoteFragment : Fragment(), MenuProvider {
     private fun addNoteToDataBase() {
         val noteTitle = binding.edAddNoteTitle.text.toString()
         val noteDesc = binding.edAddNoteDesc.text.toString()
-        val noteEditTime = getCurrentDateTime()
         if (noteTitle.isNotEmpty()) {
-            val note = Note(0, noteTitle, noteDesc, noteEditTime, noteColor)
-            viewModel.insertNote(note)
+            viewModel.addNote(noteTitle, noteDesc, noteColor)
             findNavController().popBackStack(R.id.homeFragment, false)
         } else {
             Toast.makeText(requireContext(), R.string.enter_title, Toast.LENGTH_SHORT).show()
@@ -108,13 +83,6 @@ class AddNoteFragment : Fragment(), MenuProvider {
         val date = Date()
         return dateFormat.format(date)
     }
-
-    companion object {
-
-        @JvmStatic
-        fun newInstance() = AddNoteFragment()
-    }
-
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
         menu.clear()
         menuInflater.inflate(R.menu.add_menu, menu)
@@ -127,10 +95,9 @@ class AddNoteFragment : Fragment(), MenuProvider {
                     override fun onClick(name: Int) {
                         noteColor = name
                         val imageView = binding.imageView
-                        val originalDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_launch)
-                        val color = Color.parseColor("#${Integer.toHexString(noteColor)}")
-                        originalDrawable?.colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.SRC_IN)
-                        imageView.setImageDrawable(originalDrawable)
+                        val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_launch)
+                        drawable?.setTint(noteColor)
+                        imageView.setImageDrawable(drawable)
 
                     }
                 })
